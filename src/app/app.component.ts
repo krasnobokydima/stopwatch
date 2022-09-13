d917f7bc8523a5c05eaaea9ad32056b4435ff74a
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-09-13 16:08:48 +0300

    stopwach: change logic only on rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,91 +1,140 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription, timer } from 'rxjs';
-import { MatSnackBar } from '@angular/material/snack-bar';
-
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import {
+  fromEvent,
+  map,
+  scan,
+  merge,
+  switchMap,
+  interval,
+  tap,
+  debounceTime,
+  skip,
+} from 'rxjs';
+interface State {
+  count: number;
+  timerWork: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnDestroy {
-  // stopWatch
+export class AppComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('btnStartStop', { static: true, read: ElementRef })
+  buttonStartStop!: ElementRef;
+  @ViewChild('btnWait', { static: true, read: ElementRef })
+  buttonWait!: ElementRef;
+  @ViewChild('btnReset', { static: true, read: ElementRef })
+  buttonReset!: ElementRef;
+  timerWork = false;
+
   hours = 0;
   minutes = 0;
   seconds = 0;
   mlSeconds = 0;
-  timer$: Subscription | undefined;
 
-  // delay
-  timerDelay!: ReturnType<typeof setTimeout>;
-  delay = 300;
-  prevents = false;
+  btnStartStopSubs$: any;
+  btnWaitSubs$: any;
+  btnResetSubs$: any;
+  watch$: any;
 
-  constructor(private toast: MatSnackBar) {}
+  startWatch: State = {
+    count: 0,
+    timerWork: true,
+  };
 
-  ngOnDestroy(): void { this.timer$?.unsubscribe() }
+  ngAfterViewInit(): void {
+    this.watch$ = merge(
+      fromEvent(
+        this.buttonStartStop.nativeElement as HTMLElement,
+        'click'
+      ).pipe(
+        map(() => {
+          this.timerWork = !this.timerWork;
 
-  start() {
-    if (!this.prevents) return this.error();
-    this.prevents = false;
-    clearInterval(this.timerDelay)
+          return this.timerWork
+            ? { timerWork: true }
+            : { timerWork: false, count: 0 };
+        })
+      ),
+      fromEvent(this.buttonWait.nativeElement as HTMLElement, 'click').pipe(
+        debounceTime(500),
+        skip(1),
+        map(() => {
+          this.timerWork = false;
+          return { timerWork: false };
+        })
+      ),
+      fromEvent(this.buttonReset.nativeElement as HTMLElement, 'click').pipe(
+        map(() => ({})),
+        tap(() => this.reset())
+      )
+    )
+      .pipe(
+        scan(
+          (state: State, curr: Partial<State>) => ({ ...state, ...curr }),
+          this.startWatch
+        ),
+        switchMap((state: State) => {
+          return interval(10).pipe(
+            tap(() => {
+              if (state.timerWork) {
+                state.count += 1;
+                this.changeWatchTimer();
+              }
 
-    this.timer$ = timer(0, 10).subscribe(() => this.changeWatchTimer());
+              if (!state.count && !state.timerWork) {
+                this.reset();
+              }
+            })
+          );
+        })
+      )
+      .subscribe();
   }
 
-  stop() {
-    if (!this.prevents) return this.error();
-    this.prevents = false;
-    clearInterval(this.timerDelay)
-
-    this.timer$?.unsubscribe();
-  }
-
-  reset() {
-    if (!this.prevents) return this.error();
-    this.prevents = false;
-    clearInterval(this.timerDelay)
-
-    this.timer$?.unsubscribe();
-
-    this.mlSeconds = 0;
-    this.seconds = 0;
-    this.minutes = 0;
-    this.hours = 0;
-  }
-
-  error() {
-    this.prevents = true;
-
-    this.timerDelay = setTimeout(() => {
-      this.prevents = false;
-
-      this.toast.open('Use double Click', '!!!', { duration: 1000 });
-    }, this.delay);
-  }
-
-  createWatchTimer() {
-    const prNumber = (number: number) => (number > 9 ? number : '0' + number);
-    const { hours, minutes, seconds, mlSeconds } = this;
-
-    return `${prNumber(hours)}:${prNumber(minutes)}:${prNumber(seconds)}:${prNumber(mlSeconds)}`;
+  ngOnDestroy(): void {
+    this.btnStartStopSubs$.unsubscribe();
+    this.btnWaitSubs$.unsubscribe();
+    this.btnResetSubs$.unsubscribe();
+    this.watch$.unsubscribe();
   }
 
   private changeWatchTimer() {
     this.mlSeconds += 1;
+    if (this.mlSeconds < 100) return;
+    this.mlSeconds = 0;
+    this.seconds += 1;
+    if (this.seconds < 60) return;
+    this.seconds = 0;
+    this.minutes += 1;
+    if (this.minutes < 60) return;
+    this.minutes = 0;
+    this.hours += 1;
+  }
 
-    if (this.mlSeconds === 100) {
-      this.mlSeconds = 0;
-      this.seconds += 1;
+  createWatchTimer() {
+    const { mlSeconds, seconds, minutes, hours, prepareTime } = this;
 
-      if (this.seconds === 60) {
-        this.seconds = 0;
-        this.minutes += 1;
+    return `${prepareTime(hours)}:${prepareTime(minutes)}:${prepareTime(
+      seconds
+    )}:${prepareTime(mlSeconds)}`;
+  }
 
-        if (this.minutes === 60) {
-          this.minutes = 0;
-          this.hours += 1;
-        }
-      }
-    }
+  prepareTime(count: number) {
+    return count < 10 ? '0' + count : '' + count;
+  }
+
+  private reset() {
+    this.mlSeconds = 0;
+    this.seconds = 0;
+    this.minutes = 0;
+    this.hours = 0;
   }
 }
